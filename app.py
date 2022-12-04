90407c088b2b535981f06285074fe66b580be0a1
sem1d5 <96908030+sem1d5@users.noreply.github.com>
2022-12-04 20:08:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-import os
 import openai
 import streamlit as st
 
-openai.api_key = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Generate the images
+
+st.title('DALL-E image generator')
+
+prompt = st.text_input("Enter a prompt:"
+    , value = "Couple illustration in a style of Spirited Away"
+    )
+
 response = openai.Image.create(
-  prompt="A cute baby sea otter",
+  prompt=prompt,
   n=2,
   size="1024x1024"
 )
 
-# Display the images in the app
 for image in response["data"]:
   st.image(image["url"])
